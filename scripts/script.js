d8258a7e4dd7c260598d98ec37df2d93a481173a
Yanis <133204672+YGachot@users.noreply.github.com>
2024-11-25 16:57:12 +0100

    changement complet du js et de l'html

--- scripts/script.js
+++ scripts/script.js
@@ -1,91 +1,153 @@
-async function fetchRecipes() {
-    try {
-        const response = await fetch('https://gist.githubusercontent.com/baiello/0a974b9c1ec73d7d0ed7c8abc361fc8e/raw/e598efa6ef42d34cc8d7e35da5afab795941e53e/recipes.json');
-        const recipes = await response.json();
-        displayRecipes(recipes);
+let recipes = [];
 
-        const searchInput = document.querySelector('.rechercher-input');
-        searchInput.addEventListener('input', () => filterRecipes(recipes, searchInput.value));
-    } catch (error) {
-        console.error('Erreur lors de la récupération des recettes:', error);
-    }
+fetch('https://gist.githubusercontent.com/baiello/0a974b9c1ec73d7d0ed7c8abc361fc8e/raw/e598efa6ef42d34cc8d7e35da5afab795941e53e/recipes.json')
+    .then(response => response.json())
+    .then(data => {
+        recipes = data;
+        initializeFilters();
+        displayRecipes(recipes);
+    })
+    .catch(error => console.error('Erreur de récupération des recettes:', error));
+
+
+function displayRecipes(filteredRecipes) {
+    const recipeContainer = document.getElementById('recipe-grid');
+    recipeContainer.innerHTML = '';
+
+    filteredRecipes.forEach(recipe => {
+        const recipeCard = createRecipeCard(recipe);
+        recipeContainer.appendChild(recipeCard);
+    });
+
+    updateCounter(filteredRecipes.length);
 }
 
-const imageBasePath = '/SupDeCuisine/images/Recettes/';
 
 function createRecipeCard(recipe) {
     const recipeCard = document.createElement('div');
     recipeCard.className = 'recipe-card';
 
-    const imageName = recipe.image.split('/').pop();
-    const imagePath = `${imageBasePath}${imageName}`;
-    const ingredients = recipe.ingredients;
+    const imageName = recipe.image.split('/').pop()
+    const imagePath = `/SupDeCuisine/images/Recettes/${imageName}`;
+
+
+    const ingredientsList = recipe.ingredients.map((ingredient, index) => {
+        const ingredientName = ingredient.ingredient;
+        const quantity = ingredient.quantity ? ingredient.quantity : '';
+        const unit = ingredient.unit ? ingredient.unit : ''; 
+
+    
+        const ingredientText = `${ingredientName}${quantity ? ` ${quantity}` : ''}${unit ? ` ${unit}` : ''}`;
+
+
+        return `<li><span class="ingredient" id="ingredient-${index}">${ingredientText}</span></li>`;
+    }).join('');
 
     recipeCard.innerHTML = `
-        <img src="${imagePath}" alt="${imageName}" class="recipe-image">
+        <img src="${imagePath}" alt="${recipe.name}" class="recipe-image">
         <div class="recipe-details">
             <h2 class="recipe-title">${recipe.name}</h2>
-            <br>
-            <h3 class="recipe-recipe">Recette</h3>
-            <br>
             <p class="recipe-description">${recipe.description}</p>
-            <br>
             <h3>Ingrédients:</h3>
-            <br>
-            <div class="ingredients-list">
-                ${ingredients.map((ingredient, index) => `
-                    <div class="ingredient-item ingredient-${index}">
-                        <span class="ingredient-name">${ingredient.ingredient}</span>
-                        <br>
-                        <span class="ingredient-quantity">${ingredient.quantity ? ingredient.quantity : ''}${ingredient.quantity && ingredient.unit ? ' ' : ''}${ingredient.unit ? ingredient.unit : ''}</span>
-                    </div>
-                `).join('')}
-            </div>
-
+            <ul class="ingredients-list">
+                ${ingredientsList}
+            </ul>
         </div>
     `;
-
     return recipeCard;
 }
+
+
 
 function updateCounter(count) {
     const counterElement = document.getElementById('recipe-counter');
     counterElement.textContent = `${count} Recettes`;
 }
 
-function displayErrorMessage(count, query) {
-    const errorMessageElement = document.getElementById('error-message');
-    if (count === 0) {
-        errorMessageElement.textContent = `Aucune recette ne contient '${query}'`;
-        errorMessageElement.style.display = 'block'; // Affiche le message
-    } else {
-        errorMessageElement.style.display = 'none'; // Cache le message
-    }
+
+function initializeFilters() {
+    populateFilters();
+
+    document.getElementById('ingredient-button').addEventListener('click', () => toggleFilterOptions('ingredient-options'));
+    document.getElementById('appliance-button').addEventListener('click', () => toggleFilterOptions('appliance-options'));
+    document.getElementById('utensil-button').addEventListener('click', () => toggleFilterOptions('utensil-options'));
 }
 
-function displayRecipes(recipes) {
-    const gridContainer = document.getElementById('recipe-grid');
-    gridContainer.innerHTML = '';
-    recipes.forEach(recipe => {
-        const recipeCard = createRecipeCard(recipe);
-        gridContainer.appendChild(recipeCard);
+function populateFilters() {
+    const utensilOptions = document.getElementById('utensil-options');
+    const applianceOptions = document.getElementById('appliance-options');
+    const ingredientOptions = document.getElementById('ingredient-options');
+
+    const utensilList = [...new Set(recipes.flatMap(recipe => recipe.ustensils))];
+    const applianceList = [...new Set(recipes.map(recipe => recipe.appliance))];
+    const ingredientList = [...new Set(recipes.flatMap(recipe => recipe.ingredients.map(ing => ing.ingredient)))];
+
+    populateFilterOptions(utensilOptions, utensilList);
+    populateFilterOptions(applianceOptions, applianceList);
+    populateFilterOptions(ingredientOptions, ingredientList);
+}
+
+
+function populateFilterOptions(filterElement, options) {
+    filterElement.innerHTML = '';
+    options.forEach(optionValue => {
+        const option = document.createElement('div');
+        option.className = 'filter-option';
+        option.textContent = optionValue;
+        option.addEventListener('click', () => toggleSelection(option));
+        filterElement.appendChild(option);
     });
-    updateCounter(recipes.length); 
 }
 
-function filterRecipes(recipes, query) {
-    const lowerQuery = query.toLowerCase();
+function toggleSelection(optionElement) {
+    optionElement.classList.toggle('selected');
+    applyFilters();
+}
+
+function applyFilters() {
+    const utensilFilters = Array.from(document.getElementById('utensil-options').getElementsByClassName('selected')).map(option => option.textContent);
+    const applianceFilters = Array.from(document.getElementById('appliance-options').getElementsByClassName('selected')).map(option => option.textContent);
+    const ingredientFilters = Array.from(document.getElementById('ingredient-options').getElementsByClassName('selected')).map(option => option.textContent);
+    const query = document.querySelector('.rechercher-input').value.toLowerCase().trim();
+
     const filteredRecipes = recipes.filter(recipe => {
-        const inName = recipe.name.toLowerCase().includes(lowerQuery);
-        const inDescription = recipe.description.toLowerCase().includes(lowerQuery);
-        const inIngredients = recipe.ingredients.some(ingredient =>
-            ingredient.ingredient.toLowerCase().includes(lowerQuery)
+        const matchIngredients = ingredientFilters.every(ingredient =>
+            recipe.ingredients.some(ing => ing.ingredient.toLowerCase().includes(ingredient.toLowerCase()))
         );
-        return inName || inDescription || inIngredients;
+
+        const matchAppliances = applianceFilters.length === 0 || applianceFilters.some(appliance =>
+            recipe.appliance.toLowerCase().includes(appliance.toLowerCase())
+        );
+
+        const matchUstensils = utensilFilters.length === 0 || utensilFilters.every(utensil =>
+            recipe.ustensils.some(ust => ust.toLowerCase().includes(utensil.toLowerCase()))
+        );
+
+        const matchSearch = recipe.name.toLowerCase().includes(query) ||
+            recipe.description.toLowerCase().includes(query) ||
+            recipe.ingredients.some(ing => ing.ingredient.toLowerCase().includes(query));
+
+        return matchIngredients && matchAppliances && matchUstensils && matchSearch;
     });
 
     displayRecipes(filteredRecipes);
-    displayErrorMessage(filteredRecipes.length, query); // Affiche le message d'erreur si nécessaire
+    displayErrorMessage(filteredRecipes.length, query);
 }
+
+function displayErrorMessage(count, query) {
+    const errorMessageElement = document.getElementById('error-message');
+    if (count === 0) {
+        errorMessageElement.textContent = `Aucune recette ne contient '${query}'.`;
+        errorMessageElement.style.display = 'block';
+    } else {
+        errorMessageElement.style.display = 'none';
+    }
+}
+
+function toggleFilterOptions(filterId) {
+    const filterElement = document.getElementById(filterId);
+    filterElement.style.display = (filterElement.style.display === 'block') ? 'none' : 'block';
+}
+
 
 fetchRecipes();
